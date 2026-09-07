2780fa15c175c8b440270070b0f38e211a2a3cc3
CchAcE777 <chance7melol@gmail.com>
2026-09-07 11:25:22 +0300

    n,mmkm

---
+++ ok/lol.js
@@ -0,0 +1,36 @@
+console.log('afkewb vbnob')
+
+let number = prompt('Введите число, которое вы хотите проверить')
+
+if (number < 0) {
+    console.log('Hooray, its < 0')
+} else {
+    console.log('its > 0')
+}
+
+if (number % 2 == 0) {
+    console.log('nice')
+} else {
+    console.log('not nice')
+}
+
+let first = prompt('first')
+let second = prompt('second')
+let f_count = first.at(0)
+let s_count = second.at(0)
+
+if (f_count === s_count) {
+    console.log('nice> eigornfdsgere')
+} else {
+    console.log('bebebe')
+}
+
+let last = prompt('word')
+let l_l = last.at(-1)
+let l_2 = last.at(-2)
+
+if (l_l == 'ь') {
+    console.log(l_2)
+} else {
+    console.log(l_l)
+}
